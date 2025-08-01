2562760dee21463b3b626d584f9512a180286a37
Matt Harp <matthewharp@gmail.com>
2025-08-01 17:50:22 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"
 
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 // import { Counter } from "./features/counter/Counter"
 // import { Quotes } from "./features/quotes/Quotes"
 // import logo from "./logo.svg"
 
 import Header  from "./features/header/Header.tsx"
+import AboutVision from "./features/aboutVision/AboutVision.tsx"
 
 export const App = () => (
   <div className="App">
-    <Header></Header>
-    <div style={{ backgroundColor: "#E05C42", padding: "25px" }}></div>
+    <BrowserRouter>
+      <Header></Header>
+      <h3 style={{ backgroundColor: "#E05C42", color: "#DDDDDD", padding: "30px" }}>
+        Reaching out, sharing Christ
+      </h3>
+      <div style={{ padding: "1% 20%" }}>
+        <Routes>
+          <Route path="/aboutVision" element=<AboutVision/>/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   </div>
 )
