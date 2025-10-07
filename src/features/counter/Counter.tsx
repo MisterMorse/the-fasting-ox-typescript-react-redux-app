d3e5c4837ac7face4f87db4efe1afd952c4e0fc0
Matt Harp <matthewharp@gmail.com>
2025-10-07 17:28:30 -0500

    qa

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -18,7 +18,7 @@ export const Counter = (): JSX.Element => {
   const status = useAppSelector(selectStatus)
   const [incrementAmount, setIncrementAmount] = useState("2")
 
-  const incrementValue = Number(incrementAmount) || 0
+  const incrementValue = 'aaa'
 
   return (
     <div>
