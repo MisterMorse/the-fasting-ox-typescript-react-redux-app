49f658718502ad9dd3cce63c52f5aad07b179e63
Matt Harp <matthewharp@gmail.com>
2025-12-11 15:21:23 -0600

    Built basic skeleton.
    Basic navigation framework, static content, and springboard for further development.

---
+++ src/features/aboutLeadership/AboutLeadership.tsx
@@ -0,0 +1,62 @@
+import react from "@vitejs/plugin-react"
+
+import global from "../Features.module.css"
+import styles from "./AboutLeadership.module.css"
+
+type StaffMemberProps = {
+  name: string;
+  role: string;
+  imgSource: string;
+  bio: string;
+};
+
+const StaffMember: React.FC<StaffMemberProps> = ({ name, role, imgSource, bio }) => {
+  return (
+    <div>
+      <h4>{ name }</h4>
+      <h5>{ role }</h5>
+      <div>
+        <img className={ styles.staffPic } src={ imgSource } alt="" />
+        <p>{ bio }</p>
+      </div>
+    </div>
+  );
+};
+
+function AboutLeadership() {
+  return (
+    <div>
+      <h3 className={ global.heading }>
+        Leadership
+      </h3>
+      <div className={ global.body }>
+        <StaffMember
+          name={ "Coy Howry" }
+          role={ "Pastor" }
+          imgSource={ "../../assets/images/staffCoy.jpg" }
+          bio={ "I have been a member of Lovelady Baptist Church for over 40 years, serving as pastor for the past 10 years; I am a life-long Okie, and I graduated from Allen High School and East Central University. I was raised the son of a Southern Baptist pastor, and my father served as the pastor at our church from 1980 to 1997. I became a member of Lovelady in 1982, and – after having attended Lovelady for over 30 years – I surrendered to ministry in 2013. After serving as a pulpit supply and interim pastor in area churches for two years, I was called as pastor at Lovelady. My wife Patty and I were married at Lovelady in 1984, and we have three adult children and nine grandchildren. I love spending time with my family, as well as hunting, fishing, camping, cooking, watching sports, and gardening." }
+        />
+        <StaffMember
+          name={ "Beverly Ballagh" }
+          role={ "Music Minister" }
+          imgSource={ "../../assets/images/staffBeverly.jpg" }
+          bio={ "Born and raised in northwestern Arkansas, I graduated from Alma High School in 1976; I began taking piano lessons when I was eight years old and continued them throughout my high school years, being forced to play in piano contests and recitals (I did not enjoy those at all). After high school graduation, I attended a junior college in Fort Smith, continuing to take piano lessons and sing in the choir. From there, I transferred to Oklahoma Baptist University in Shawnee, where I continued my piano and vocal studies; I graduated in 1980 with a bachelor’s degree in music education, with a choral emphasis. Upon graduation, I had a job awaiting me at Vanoss School as a music teacher for grades 1-12; this is where I met my husband. We have been married for 43 years and have three children and seven grandchildren. I worked some odd jobs while staying home raising our children but have always been a church accompanist wherever I attended, beginning at age 14. I also worked in the Ada school system as an accompaniment to the junior and senior high choirs, as well as performing arts for 10 years. I became music director at Lovelady about six years ago, initially until another music director could be found. It is such a blessing leading the music here at Lovelady. The congregation is so wonderful and supportive, and I have some of the best musicians working with me – including our amazing, talented church members that sing or play specials for our service." }
+        />
+        <StaffMember
+          name={ "Angie Smith" }
+          role={ "Children's Minister" }
+          imgSource={ "../../assets/images/staffAngie.jpg" }
+          bio={ "I am married to Duane Smith, with whom I have two children: Trace and Kylee; I graduated from Byng High School and East Central University, and I have attended Lovelady nearly all my life. I began keeping the nursery when I was 12 and continued to be involved with the children’s ministry – teaching children’s church, working at VBS, and directing children’s Christmas programs. Then, in 2000, the opportunity came to accept the children's minister position. The position covers children’s church each Sunday morning and children’s class on Wednesday nights; additionally, I help with activities throughout the year, like Vacation Bible School and Falls Creek Children’s Camp. I even correspond with children for their birthdays, attend their activities, and visit when they are sick or in the hospital." }
+        />
+        <StaffMember
+          name={ "Tyler Ware" }
+          role={ "Youth Minister" }
+          imgSource={ "../../assets/images/staffTyler.jpg" }
+          bio={ "I was born and raised in Ada, OK; I am a graduate of Byng Public School and am currently attending East Central University, majoring in history with a minor in education. I got my start in youth ministry after being called while working on a college campus." }
+        />
+      </div>
+    </div>
+  )
+}
+
+export default AboutLeadership;
---
+++ src/features/aboutVisit/AboutVisit.tsx
@@ -0,0 +1,29 @@
+import global from "../Features.module.css"
+
+import pair from "../../assets/images/pair.jpg"
+
+function AboutVisit() {
+  return (
+    <div>
+      <h3 className={ global.heading }>
+        Planning Your Visit
+      </h3>
+      <div className={ global.body }>
+        <img src={ pair } style={{ float: "left",  marginRight: "15px", marginBottom: "15px", width: "30%", height: "auto" }} alt="" />
+        We know that meeting someone for the first time can be intimidating, and going to a new church for the first time can be nerve-wracking. We want to help make your first experience at Lovelady Baptist a great one!
+        <div style={{ marginTop: "25px" }}>
+          <p style={{ fontWeight: "bold" }}>What time is the main service?</p>
+          <p>Sunday at 11:00AM</p>
+          <p style={{ fontWeight: "bold" }}>How long is a service at Lovelady Baptist?</p>
+          <p>In total, a service is about 70 minutes in length. Our worship service begins at 11:00AM; song lyrics are projected on the screen so you can sing along and/or engage with worship however you feel most comfortable. After the music portion of service is complete, our pastor will share an encouraging and hope-filled message about Jesus!</p>
+          <p style={{ fontWeight: "bold" }}>What's the culture like at Lovelady Baptist?</p>
+          <p>Sundays at Lovelady Baptist are exciting, casual, and relaxed. Come as you are, and expect to feel as our guest.</p>
+          <p style={{ fontWeight: "bold" }}>What about my kids?</p>
+          <p>We believe that kids should have a blast at church every single week, and we make this a priority. At Lovelady Baptist, the children from age three through grade six will go to children's church after our morning songs; nursery workers are also available for infants. Your children's safely is a top priority, and everyone who works with the children has passed a background check.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutVisit;
