3c88f99d919d4e05514e719a6aae83e8c342f383
Matt Harp <matthewharp@gmail.com>
2025-12-11 15:21:23 -0600

    Basic skeleton
    Basic navigation framework, static content, and springboard for further development.

---
+++ src/features/connectRequests/ConnectRequests.tsx
@@ -0,0 +1,15 @@
+import global from "../Features.module.css"
+
+function ConnectRequests() {
+  return (
+    <div>
+      <h3 className={ global.heading }>
+        Prayer Requests
+      </h3>
+      <div className={ global.body }>
+      </div>
+    </div>
+  )
+}
+
+export default ConnectRequests;
