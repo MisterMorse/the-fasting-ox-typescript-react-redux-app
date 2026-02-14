5f08976cd218b66576f5822ca7106e836f8c59c2
Matt Harp <79379521+MisterMorse@users.noreply.github.com>
2026-02-14 16:51:43 -0600

    Setup for Nginx and tweaks.
    Basic implementation of Nginx and associated Docker config.

--- src/features/events/EventsAccordion.tsx
+++ src/features/events/EventsAccordion.tsx
@@ -8,7 +8,7 @@ const EventsAccordion = (category: string): JSX.Element | null => {
     return (
       <div>
         <h5>
-          Whoops! Error loading events.
+          { import.meta.env.VITE_MESSAGE_EVENTS_FAILURE }
         </h5>
       </div>
     )
@@ -18,31 +18,42 @@ const EventsAccordion = (category: string): JSX.Element | null => {
     return (
       <div>
         <h5>
-          Loading events...
+          { import.meta.env.VITE_MESSAGE_EVENTS_LOADING }
         </h5>
       </div>
     )
   }
 
   if (isSuccess) {
-    return (
-      <div>
-        {data.events.map(({ id, name, description, location, date, start_time, end_time, day_of_week}) => (
-          <div>
-            <h3>
-              { name }
-            </h3>
-            <h5>
-              { date }, { start_time } - { end_time }<br/>
-              { location }
-            </h5>
-            <p>
-              { description }
-            </p>
-          </div>
-        ))}
-      </div>
-    )
+    if (data.events.length > 0) {
+      return (
+        <div>
+          { data.events.map(({ id, name, description, location, date, start_time, end_time, day_of_week }) => (
+            <div key={ id }>
+              <h3>
+                { name }
+              </h3>
+              <h5>
+                { day_of_week }, { date }
+                { start_time } - { end_time }
+                { location }
+              </h5>
+              <p>
+                { description }
+              </p>
+            </div>
+          )) }
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h5>
+            { import.meta.env.VITE_MESSAGE_EVENTS_EMPTY }
+          </h5>
+        </div>
+      )
+    }
   }
 
   return null
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
 
 type ImportMetaEnv = {
-  readonly VITE_API_URL: string;
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_ERROR_EVENT_LOADING: string
+  readonly VITE_ERROR_EVENT_EMPTY: string
+  readonly VITE_ERROR_EVENT_FAILURE: string
 }
 
 type ImportMeta = {
