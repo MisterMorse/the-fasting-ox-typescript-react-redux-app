f8f3b4b61abc90ede76783da599f9f4476f115b0
Matt Harp <79379521+MisterMorse@users.noreply.github.com>
2026-02-14 16:28:12 -0600

    a

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 type ImportMetaEnv = {
   readonly VITE_API_BASE_URL: string
+  readonly VITE_ERROR_EVENT_LOADING: string
   readonly VITE_ERROR_EVENT_EMPTY: string
   readonly VITE_ERROR_EVENT_FAILURE: string
 }
